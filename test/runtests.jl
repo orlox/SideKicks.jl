2ae2e0aaed9df84e70444008b3932d5f12328979
MatsEsseldeurs <matse@wezen.ster.kuleuven.be>
2022-03-16 13:35:08 +0100

    Additional testing

--- test/runtests.jl
+++ test/runtests.jl
@@ -43,6 +43,134 @@ RV_s = SideKicks.RV_semiamplitude_K(year_in_days,0,π/2,1,m_earth/m_sun)
 @test abs(RV_e-29.7846) < 0.001 #just test agains value that was computed in a trusted version
 @test isapprox(RV_e/RV_s, m_sun/m_earth) #test that RV ratio follows mass ratio
 
+#Verify consistency of variables upon no mass loss and no kick
+SideKicks.create_symbolic_functions_list()
+m1 = 10
+m2 = 15
+m2f = 15
+P_i = 50 # days
+a_i = SideKicks.kepler_a_from_P(P_i,m1,m2)
+vkick = 0
+θ = 2
+ϕ = 1
+Ω_i=3
+ω_i=6
+ι_i=0.4
+e_i=0.4
+ν=2
+
+a_f, e_f, v_N, v_E, v_r, Ω_f, ω_f, ι_f = SideKicks.generalized_post_kick_parameters_a_e(
+    a_i,e_i,sin(ν),cos(ν),m1*m_sun,m2*m_sun,m2f*m_sun,vkick,
+    sin(θ),cos(θ),sin(ϕ),cos(ϕ),sin(Ω_i),cos(Ω_i),sin(ω_i),cos(ω_i),sin(ι_i),cos(ι_i),SideKicks.symbolic_functions_list)
+P_f = SideKicks.kepler_P_from_a(a_f,m1,m2f)
+
+@test isapprox(P_i, P_f) #test that the period remains the same
+@test isapprox(a_i, a_f) #test that the semi-major axis remains the same
+@test isapprox(e_i, e_f, atol=1e-7) #test that the eccentricity remains the same
+@test isapprox(v_N, 0.0, atol=1e-10) #test that the velocity is zero
+@test isapprox(v_E, 0.0, atol=1e-10) #test that the velocity is zero
+@test isapprox(v_r, 0.0, atol=1e-10) #test that the velocity is zero
+@test isapprox(Ω_i, Ω_f) #test that Ω remains the same
+@test isapprox(ω_i, ω_f) #test that ω remains the same
+@test isapprox(ι_i, ι_f) #test that ι remains the same
+
+#Verify consistency of variables upon reversing a kick
+# SideKicks.create_symbolic_functions_list()
+# m1 = 10
+# m2 = 15
+# m2f = m2
+# P_i = 50 # days
+# a_i = SideKicks.kepler_a_from_P(P_i,m1,m2)
+# vkick = 100e5
+# θ = 2
+# ϕ = 1
+# Ω_i=3
+# ω_i=6
+# ι_i=0.4
+# e_i=0
+# ν=0
+
+# a_f1, e_f1, v_N1, v_E1, v_r1, Ω_f1, ω_f1, ι_f1 = SideKicks.generalized_post_kick_parameters_a_e(
+#     a_i,e_i,sin(ν),cos(ν),m1*m_sun,m2*m_sun,m2f*m_sun,vkick,
+#     sin(θ),cos(θ),sin(ϕ),cos(ϕ),sin(Ω_i),cos(Ω_i),sin(ω_i),cos(ω_i),sin(ι_i),cos(ι_i),SideKicks.symbolic_functions_list)
+
+# a_f, e_f, v_N, v_E, v_r, Ω_f, ω_f, ι_f = SideKicks.generalized_post_kick_parameters_a_e(
+#     a_f1,e_f1,sin(ν),cos(ν),m1*m_sun,m2*m_sun,m2f*m_sun,-vkick,
+#     sin(θ),cos(θ),sin(ϕ),cos(ϕ),sin(Ω_f1),cos(Ω_f1),sin(ω_f1),cos(ω_f1),sin(ι_f1),cos(ι_f1),SideKicks.symbolic_functions_list)
+# P_f = SideKicks.kepler_P_from_a(a_f,m1,m2f)
+
+# @test isapprox(P_i, P_f, rtol=1e-5) #test that the period remains the same
+# @test isapprox(a_i, a_f, rtol=1e-5) #test that the semi-major axis remains the same
+# @test isapprox(e_i, e_f, atol=1e-7) #test that the eccentricity remains the same
+# @test isapprox(v_N1, -v_N, atol=1e-10) #test that the velocity is zero
+# @test isapprox(v_E1, -v_E, atol=1e-10) #test that the velocity is zero
+# @test isapprox(v_r1, -v_r, atol=1e-10) #test that the velocity is zero
+# @test isapprox(Ω_i, Ω_f) #test that Ω remains the same
+# @test isapprox(ι_i, ι_f) #test that ι remains the same
+
+#Verify reversing orbital velocity, with mass loss (Hills 1982)
+SideKicks.create_symbolic_functions_list()
+m1 = 10
+m2 = 15
+m2f = 10
+P_i = 50 # days
+a_i = SideKicks.kepler_a_from_P(P_i,m1,m2)
+θ = π
+ϕ = 0
+Ω_i=3
+ω_i=6
+ι_i=0.4
+e_i=0.5
+ν=0 #TODO: DOES NOT WORK FOR ALL VALUES OF ν
+r_i = a_i*(1-abs2(e_i))/(1+e_i*cos(ν))
+vkick = sqrt(cgrav*(m1+m2)*m_sun/a_i)*2*sqrt(2*a_i/r_i-1)
+
+a_f1, e_f1, v_N1, v_E1, v_r1, Ω_f1, ω_f1, ι_f1 = SideKicks.generalized_post_kick_parameters_a_e(
+    a_i,e_i,sin(ν),cos(ν),m1*m_sun,m2*m_sun,m2f*m_sun,0,
+    sin(θ),cos(θ),sin(ϕ),cos(ϕ),sin(Ω_i),cos(Ω_i),sin(ω_i),cos(ω_i),sin(ι_i),cos(ι_i),SideKicks.symbolic_functions_list)
+P_f1 = SideKicks.kepler_P_from_a(a_f1,m1,m2f)
+
+a_f2, e_f2, v_N2, v_E2, v_r2, Ω_f2, ω_f2, ι_f2 = SideKicks.generalized_post_kick_parameters_a_e(
+    a_i,e_i,sin(ν),cos(ν),m1*m_sun,m2*m_sun,m2f*m_sun,vkick,
+    sin(θ),cos(θ),sin(ϕ),cos(ϕ),sin(Ω_i),cos(Ω_i),sin(ω_i),cos(ω_i),sin(ι_i),cos(ι_i),SideKicks.symbolic_functions_list)
+P_f2 = SideKicks.kepler_P_from_a(a_f2,m1,m2f)
+
+@test isapprox(P_f1, P_f2, rtol=1e-5) #test that the period remains the same
+@test isapprox(a_f1, a_f2, rtol=1e-5) #test that the semi-major axis remains the same
+@test isapprox(e_f1, e_f2, atol=1e-7) #test that the eccentricity remains the same
+@test isapprox(Ω_f1+π, Ω_f2) || isapprox(Ω_f1-π, Ω_f2) #test that Ω shifts a factor of π
+@test isapprox(ι_f1, ι_f2) #test that ι remains the same
+
+#Verify vanishing orbital velocity, with mass loss (Hills 1982)
+SideKicks.create_symbolic_functions_list()
+m1 = 10
+m2 = 15
+m2f = 10
+P_i = 50 # days
+a_i = SideKicks.kepler_a_from_P(P_i,m1,m2)
+θ = π
+ϕ = 0
+Ω_i=3
+ω_i=6
+ι_i=0.4
+e_i=0.5
+ν=0 #TODO: DOES NOT WORK FOR ALL VALUES OF ν
+r_i = a_i*(1-abs2(e_i))/(1+e_i*cos(ν))
+vkick = sqrt(cgrav*(m1+m2)*m_sun/a_i)*sqrt(2*a_i/r_i-1)
+
+a_f, e_f, v_N, v_E, v_r, Ω_f, ω_f, ι_f = SideKicks.generalized_post_kick_parameters_a_e(
+    a_i,e_i,sin(ν),cos(ν),m1*m_sun,m2*m_sun,m2f*m_sun,vkick,
+    sin(θ),cos(θ),sin(ϕ),cos(ϕ),sin(Ω_i),cos(Ω_i),sin(ω_i),cos(ω_i),sin(ι_i),cos(ι_i),SideKicks.symbolic_functions_list)
+P_f = SideKicks.kepler_P_from_a(a_f,m1,m2f)
+
+@test isapprox(a_f, r_i/2) #test that the eccentricity becomes 1
+@test isapprox(1., e_f, atol=1e-7) #test that the eccentricity becomes 1
+@test isapprox(Ω_i, Ω_f) || isapprox(Ω_i+π, Ω_f) || isapprox(Ω_i-π, Ω_f) #test that Ω remains the same or flips
+@test isapprox(ι_i, ι_f) #test that ι remains the same
+
+
+
+
 ##do sampling on a simplified problem to see if results are consistent
 ##use binary system tested above with narrow priors on kick direction and fraction of
 ##mass loss to make problem invertible
