8f1d5f3654f39bc5fd3d843852c3eb0f58762041
Reinhold Willcox <reinhold.willcox@gmail.com>
2024-04-17 15:36:31 +0200

    started docs for BHModels

--- src/BHModels.jl
+++ src/BHModels.jl
@@ -1,11 +1,40 @@
 """
-Test documentation
-"""
+    arbitraryEjectaBH(m2_i, frac)
 
+#TODO Description
+#RTW do we really need this function? It seems like overkill for such a simple operation
+
+# Arguments:
+#TODO
+- m2_i:
+- frac:
+
+# Output:
+#TODO
+- 
+"""
 function arbitraryEjectaBH(m2_i, frac)
     return m2_i*(1-frac)
 end
 
+"""
+    restrictedEjectaBH(m2_i, frac; Ma=10, Mb=15, max_frac=1.0, min_frac=0.1)
+
+#TODO Description
+
+# Arguments:
+#TODO
+- m2_i:
+- frac:
+- Ma=10:
+- Mb=15:
+- max_frac=1.0:
+- min_frac=0.1:
+
+# Output:
+#TODO
+- 
+"""
 function restrictedEjectaBH(m2_i, frac; Ma=10, Mb=15, max_frac=1.0, min_frac=0.1)
     frac_limit = max_frac
     if m2_i > Ma && m2_i < Mb
