fb438809814c1b5f467e87d4eb239a01c7979f09
Reinhold Willcox <reinhold.willcox@gmail.com>
2024-08-13 13:16:06 +0200

    last changes

--- examples/run_inference.jl
+++ examples/run_inference.jl
@@ -6,17 +6,18 @@ using HDF5
 
 # USER sets this! - Ensure this matches a directory in examples/ which has the appropriate files and filenames
 system_id = "vfts243" 
+system_dir = String(@__DIR__)*"/$system_id/"
 
 ## 
 
 # Obtain observations and priors from external files
-obs_string = open("examples/$system_id/observations.jl", "r") do file
+obs_string = open(system_dir * "observations.jl", "r") do file
         read(file, String)
 end
 obs = eval(Meta.parse(obs_string))
 
 
-priors_string = open("examples/$system_id/priors.jl", "r") do file
+priors_string = open(system_dir * "priors.jl", "r") do file
     read(file, String)
 end
 priors = eval(Meta.parse(priors_string))
@@ -36,11 +37,11 @@ mcmcStruct = SideKicks.RunKickMCMC(
         which_model = which_model,
         observations = obs,
         priors = priors,
-        nuts_warmup_count = 20000,
+        nuts_warmup_count = 200,
         nuts_acceptance_rate = 0.8,
-        nsamples = 200,
+        nsamples = 2000,
         nchains = 8)
 
 ##
 
-SideKicks.SaveResults("examples/$system_id/results.hdf5", mcmcStruct, obs_string, priors_string)
+SideKicks.SaveResults(system_dir *"results-medium.hdf5", mcmcStruct, obs_string, priors_string)
